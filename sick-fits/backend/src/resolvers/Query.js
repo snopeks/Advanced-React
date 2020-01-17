1d94a9a4c9129b3427f8556c43564a009df4a51b
snopeks <creativesnopeks@gmail.com>
2020-01-17 11:51:42 -0800

    completed video 13

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+// this is where your database calls will go regardless of what db you are using on the backend. 
+const Query = {
+    dogs(parent, args, ctx, info){
+        global.dogs = global.dogs || [];
+        return global.dogs;
+    }
+
+};
 
 module.exports = Query;
