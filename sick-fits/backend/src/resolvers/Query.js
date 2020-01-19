714642170765f48e365c922a8dc7c1c51c216a46
snopeks <creativesnopeks@gmail.com>
2020-01-19 14:46:31 -0800

    completed video 14

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,9 +1,12 @@
 // this is where your database calls will go regardless of what db you are using on the backend. 
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-    dogs(parent, args, ctx, info){
-        global.dogs = global.dogs || [];
-        return global.dogs;
-    }
+    items: forwardTo('db'), 
+    // async items(parent, args, ctx, info){
+    //     const items = await ctx.db.query.items();
+    //     return items; 
+    // }
 
 };
 
